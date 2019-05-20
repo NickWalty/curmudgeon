a9aec5c701022a326f3396d57a998372629b3f99
Nicholas Bell <nicholas@waltechvis.com>
2019-05-20 16:19:53 -0300

    Updated url parsing to work with Slack Bot

--- background.js
+++ background.js
@@ -1,12 +1,17 @@
+
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-    	let incomingURL    = details.url.split('/');
+        let incomingURL    = details.url.split('/');
         let incomingParams = details.url.split('?');
-    	if ( incomingURL[incomingURL.length -2] !== 'browse' || incomingParams.includes('oldIssueView=true') ) {
-    		return {redirectUrl: details.url}; //Don't add parameters
-    	} 
-    	let newUrl = details.url + '?oldIssueView=true';
-    	return {redirectUrl: newUrl};
+        if ( incomingURL[incomingURL.length -2] !== 'browse' || incomingParams.includes('oldIssueView=true') ) {
+            return {redirectUrl: details.url}; //Don't add parameters
+        } 
+        if ( incomingParams.length > 1 && !incomingParams.includes('oldIssueView=true')) {
+            let newUrl = details.url + '&oldIssueView=true';
+            return {redirectUrl: newUrl};
+        }
+        let newUrl = details.url + '?oldIssueView=true';
+        return {redirectUrl: newUrl};
     },
     {urls: ["*://starlingsolutions.atlassian.net/*"]},
     ["blocking"]);
